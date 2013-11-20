fada2f37407838907697f919ec9c0c6cdf99f5a0
Enzo Vidal <exirion@gmail.com>
2013-11-20 20:06:30 -0300

    Validacion ok

--- index.php
+++ index.php
@@ -5,13 +5,57 @@
 </head>
 <body>
 	
-	<form action="inscribir.php">
+	<form name="formulario" action="inscribir.php" method="POST">
 		Nombre: <input type="text" name="nombre"><br>
 		Usuario: <input type="text" name="usuario"><br>
 		Clave: <input type="password" name="clave"><br>
-		<input type="button" value="Inscribir" onclick="">
+		Reingrese clave: <input type="password" name="clave2"><br>
+		<input type="button" value="Inscribir" onclick="validar();">
 	</form>
+	
+	<script type="text/javascript">
+		function validar()
+		{
+			if (document.formulario.nombre.value.length==0)
+					{
+						alert("Falta nombre");
+						document.formulario.nombre.focus();
+						return 0;
+					};
 
+			if (document.formulario.usuario.value.length==0)
+					{
+						alert("Falta usuario");
+						document.formulario.usuario.focus();
+						return 0;
+					};
+
+			if (document.formulario.clave.value.length==0)
+					{
+						alert("Falta clave");
+						document.formulario.clave.focus();
+						return 0;
+					};
+
+			if (document.formulario.clave2.value.length==0)
+					{
+						alert("Reingrese Clave");
+						document.formulario.clave2.focus();
+						return 0;
+					};
+
+			if (document.formulario.clave.value!=document.formulario.clave2.value)
+					{
+						alert("Las claves no coinciden");
+							document.formulario.clave.focus();
+							return 0;
+					};
+
+
+			alert("Todo OK");
+
+		}
+	</script>
 
 </body>
 </html>
