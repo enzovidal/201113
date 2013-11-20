fb0f20e8e86e3d3f631fbbaf563fec91d310bc74
Enzo Vidal <exirion@gmail.com>
2013-11-20 19:47:42 -0300

    Clase

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Repaso</title>
+</head>
+<body>
+	
+	<form action="inscribir.php">
+		Nombre: <input type="text" name="nombre"><br>
+		Usuario: <input type="text" name="usuario"><br>
+		Clave: <input type="password" name="clave"><br>
+		<input type="button" value="Inscribir" onclick="">
+	</form>
+
+
+</body>
+</html>
