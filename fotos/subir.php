d5eb07052744d385ef2dfa195585b1c3ba3a8dc7
Enzo Vidal <exirion@gmail.com>
2013-11-20 22:19:38 -0300

    Final

--- fotos/subir.php
+++ fotos/subir.php
@@ -2,7 +2,17 @@
 $archivo = $_FILES['foto']['tmp_name'];
 $nombreArchivo = $_FILES['foto']['name'];
 $ruta = "imagenes";
+$idFoto = rand(0,200);
+$nombreArchivo = $idFoto."-".$nombreArchivo;
 
 move_uploaded_file($archivo,$ruta."/".$nombreArchivo);
+$ruta=$ruta."/".$nombreArchivo;
+
+$link = mysql_connect('localhost','root','root');
+mysql_select_db('201113');
+mysql_query("INSERT INTO fotos VALUES('','$ruta')");
+mysql_close();
+
+echo "<img src=".$ruta.">";
 
 ?>
